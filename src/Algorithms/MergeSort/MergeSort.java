f8d76e3e9c5c60ad48b5488bb123f653639bbcb0
Maksym Novikov <maksymn@suntechinnovation.com>
2021-01-11 10:38:36 +0200

    updated merge sort with newer version

--- src/Algorithms/MergeSort/MergeSort.java
+++ src/Algorithms/MergeSort/MergeSort.java
@@ -1,117 +1,78 @@
 package Algorithms.MergeSort;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.text.DecimalFormat;
+import static Helper.AlgorithmsHelper.*;
 
 public class MergeSort {
   public static void main(String[] args) {
-    int[] testArray = new int[] {86, 52, 11, 34, 7, 12, 95, 72};
-    int[] sortedArray = divide(testArray);
+    int[] testArray = new int[] {100, 55, 67, 73, 14, 97, 1, 12};
+    int[] filledArray = fillArray(10_000_000);
 
-    printArray(sortedArray);
+    testMergeSort(filledArray);
   }
 
   public static int[] divide(int[] array) {
-    if (array.length < 2) {
-      return array;
-    }
+    if (array.length < 2) return array;
 
-    int leftIndex = 0;
-    int rightIndex = array.length - 1;
-    int middleIndex = (leftIndex + rightIndex) / 2;
+    int middleIndex = (array.length - 1) / 2;
 
-    int[] leftArray = getElementsFromArray(array, leftIndex, middleIndex);
-    int[] rightArray = getElementsFromArray(array, middleIndex + 1, rightIndex);
+    int[] leftArray = getArrayFrom(array, 0, middleIndex);
+    int[] rightArray = getArrayFrom(array, middleIndex + 1, array.length - 1);
 
-    int[] dividedLeftArray = divide(leftArray);
-    int[] dividedRightArray = divide(rightArray);
+    leftArray = divide(leftArray);
+    rightArray = divide(rightArray);
 
-    return mergeUsingList(dividedLeftArray, dividedRightArray);
+    return merge(leftArray, rightArray);
   }
 
-  public static int[] getElementsFromArray(int[] array, int indexFrom, int indexTo) {
-    List<Integer> list = new ArrayList<>();
-    for (int i = indexFrom; i <= indexTo; i++) {
-      list.add(array[i]);
+  public static int[] getArrayFrom(int[] array, int startIndex, int endIndex) {
+    int[] newArray = new int[endIndex - startIndex + 1];
+    int index = 0;
+
+    for (int i = startIndex; i <= endIndex; i++) {
+      newArray[index] = array[i];
+      index++;
     }
 
-    return list.stream().mapToInt(i -> i).toArray();
+    return newArray;
   }
 
-  public static int[] merge(int[] firstArray, int[] secondArray) {
-    int firstArrayLength = firstArray.length;
-    int secondArrayLength = secondArray.length;
-    int firstArrayIterator = 0;
-    int secondArrayIterator = 0;
-    List<Integer> list = new ArrayList<>();
+  public static int[] merge(int[] leftArray, int[] rightArray) {
+    int[] sortedArray = new int[leftArray.length + rightArray.length];
+    int leftArrayIndex = 0;
+    int rightArrayIndex = 0;
 
-    while (firstArrayIterator < firstArrayLength || secondArrayIterator < secondArrayLength) {
-      if (firstArrayIterator == firstArrayLength) {
-        list.add(secondArray[secondArrayIterator]);
-        secondArrayIterator++;
-        continue;
-      }
-
-      if (secondArrayIterator == secondArrayLength) {
-        list.add(firstArray[firstArrayIterator]);
-        firstArrayIterator++;
-        continue;
-      }
-
-      int firstArrayElement = firstArray[firstArrayIterator];
-      int secondArrayElement = secondArray[secondArrayIterator];
-
-      if (firstArrayElement < secondArrayElement) {
-        list.add(firstArrayElement);
-        firstArrayIterator++;
+    for (int i = 0; i < sortedArray.length; i++) {
+      if (leftArrayIndex >= leftArray.length) {
+        sortedArray[i] = rightArray[rightArrayIndex];
+        rightArrayIndex++;
+      } else if (rightArrayIndex >= rightArray.length) {
+        sortedArray[i] = leftArray[leftArrayIndex];
+        leftArrayIndex++;
       } else {
-        list.add(secondArrayElement);
-        secondArrayIterator++;
+        int leftArrayElement = leftArray[leftArrayIndex];
+        int rightArrayElement = rightArray[rightArrayIndex];
+
+        if (leftArrayElement < rightArrayElement) {
+          sortedArray[i] = leftArrayElement;
+          leftArrayIndex++;
+        } else {
+          sortedArray[i] = rightArrayElement;
+          rightArrayIndex++;
+        }
       }
     }
 
-    return list.stream().mapToInt(i -> i).toArray();
+    return sortedArray;
   }
 
-  public static int[] mergeUsingList(int[] firstArray, int[] secondArray) {
-    List<Integer> firstList = Arrays.stream(firstArray)
-      .boxed()
-      .collect(Collectors.toList());
-    List<Integer> secondList = Arrays.stream(secondArray)
-      .boxed()
-      .collect(Collectors.toList());
+  public static void testMergeSort(int[] array) {
+    long start = System.currentTimeMillis();
+    divide(array);
+    long end = System.currentTimeMillis();
 
-    List<Integer> returnList = new LinkedList<>();
-
-    while (firstList.size() > 0 || secondList.size() > 0) {
-      if (firstList.isEmpty()) {
-        returnList.add(secondList.get(0));
-        secondList.remove(0);
-        continue;
-      }
-
-      if (secondList.isEmpty()) {
-        returnList.add(firstList.get(0));
-        firstList.remove(0);
-        continue;
-      }
-
-      int firstListItem = firstList.get(0);
-      int secondListItem = secondList.get(0);
-
-      if (firstListItem < secondListItem) {
-        returnList.add(firstListItem);
-        firstList.remove(0);
-      } else {
-        returnList.add(secondListItem);
-        secondList.remove(0);
-      }
-    }
-
-    return returnList.stream().mapToInt(i -> i).toArray();
-  }
-
-  public static void printArray(int[] array) {
-    System.out.println(Arrays.toString(array));
+    System.out.println("Merge sort");
+    System.out.println("Array length: " + new DecimalFormat("#,###").format(array.length));
+    System.out.println(String.format("Sorted time: %dms", end - start));
   }
 }
