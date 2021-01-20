7dc26414d9cd31f3a3068c6fb62e2b285c9a6f9a
Maksym Novikov <maksymn@suntechinnovation.com>
2021-01-20 15:22:14 +0200

    fixed get() in LinkedList()

--- src/Helper/AlgorithmsHelper.java
+++ src/Helper/AlgorithmsHelper.java
@@ -3,21 +3,21 @@ package Helper;
 import java.util.Arrays;
 
 public class AlgorithmsHelper {
+
   public static void printArray(int[] array) {
     System.out.println(Arrays.toString(array));
   }
 
   public static int[] fillArray(int quantity) {
     int[] returnedArray = new int[quantity];
-
     for (int i = 0; i < returnedArray.length; i++) {
       returnedArray[i] = getRandomNumber(quantity);
     }
-
     return returnedArray;
   }
 
   public static int getRandomNumber(int quantity) {
     return (int) (Math.random() * (quantity + 1));
   }
+
 }
