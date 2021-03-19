17051c9f06a56d98b36c1e2b0cd0652a76c68135
Maksym Novikov <maksymn@suntechinnovation.com>
2021-03-19 22:52:43 +0200

    read all english words and put them to map

--- src/Tasks_from_work/Hash_Map_Task/Main.java
+++ src/Tasks_from_work/Hash_Map_Task/Main.java
@@ -1,17 +1,75 @@
 package Tasks_from_work.Hash_Map_Task;
 
+import java.io.*;
+import java.util.ArrayList;
+import static Tasks_from_work.Hash_Map_Task.Translator.*;
+
 public class Main {
-  public static void main(String[] args) {
-    HashMapWrapper hashMap = new HashMapWrapper();
+  static HashMapWrapper hashMap = new HashMapWrapper();
 
-    hashMap.add("5852", "luck");
-    hashMap.add("5852", "juck");
-    hashMap.add("27753", "apple");
-    hashMap.add("6262", "nana");
-    hashMap.add("672643", "orange");
-    hashMap.add("5852", "kuck");
-    hashMap.add("6262", "obob");
+  public static void main(String[] args) throws IOException {
+    putWordToHashMap(readFromNumbersCodeFile());
 
-    System.out.println(hashMap.get("5852"));
+    // change numbersCode here
+    System.out.println(hashMap.get("3629"));
   }
+
+  private static void putWordToHashMap(ArrayList<String> arrayList) {
+    for (int i = 0; i < arrayList.size(); i++) {
+      hashMap.add(translateToNumber(arrayList.get(i)), arrayList.get(i));
+    }
+  }
+
+  private static ArrayList<String> readFromNumbersCodeFile() {
+    ArrayList<String> words = new ArrayList<>();
+
+    try {
+      File file = new File("src/Tasks_from_work/Hash_Map_Task/new_words.txt");
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+      String line;
+
+      while ((line = bufferedReader.readLine()) != null) {
+        if (line.matches("[a-zA-Z]+")) {
+          words.add(line.toLowerCase());
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return words;
+  }
+
+  // private static ArrayList<String> readFromNumbersCodeFile() {
+  //   ArrayList<String> fileLines = new ArrayList<>();
+  //
+  //   try {
+  //     File file = new File("src/Tasks_from_work/Hash_Map_Task/words.txt");
+  //     BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+  //     String line;
+  //
+  //     while ((line = bufferedReader.readLine()) != null) {
+  //       if (line.matches("[a-zA-Z]+")) {
+  //         fileLines.add(line.toLowerCase());
+  //       }
+  //     }
+  //   } catch (Exception e) {
+  //     e.printStackTrace();
+  //   }
+  //
+  //   return fileLines;
+  // }
+  //
+  // public static void writeFile4(ArrayList<String> arrayList) throws IOException {
+  //   File fout = new File("src/Tasks_from_work/Hash_Map_Task/new_words.txt");
+  //   FileOutputStream fos = new FileOutputStream(fout);
+  //
+  //   OutputStreamWriter osw = new OutputStreamWriter(fos);
+  //
+  //   for (int i = 0; i < arrayList.size(); i++) {
+  //     osw.write(arrayList.get(i) + "\n");
+  //   }
+  //
+  //   osw.close();
+  // }
 }
