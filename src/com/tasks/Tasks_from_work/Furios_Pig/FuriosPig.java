9289abc2aede6d12cb51cef02d4645bf4823c3c7
Maksym Novikov <maksymn@suntechinnovation.com>
2021-03-31 22:23:38 +0300

    furious-pig: basic setup

---
+++ src/com/tasks/Tasks_from_work/Furios_Pig/FuriosPig.java
@@ -0,0 +1,59 @@
+package com.tasks.Tasks_from_work.Furios_Pig;
+
+import java.util.Random;
+
+public class FuriosPig {
+  private int positiveN;
+  private int negativeN;
+  private int stepsCounter = 0;
+  private int pigPosition = 0;
+  private final int MAX_STEPS = 10_000_000;
+
+  FuriosPig() {}
+
+  FuriosPig(int n) {
+    this.positiveN = Math.abs(n);
+    this.negativeN = -Math.abs(n);
+  }
+
+  public void escape() {
+    while (!hasPigEscaped()) {
+      movePig();
+    }
+  }
+
+  private void movePig() {
+    Random random = new Random();
+
+    if (random.nextBoolean()) {
+      goLeft();
+      printStepData();
+    } else {
+      goRight();
+      printStepData();
+    }
+  }
+
+  private void goLeft() {
+    stepsCounter++;
+    pigPosition--;
+  }
+
+  private void goRight() {
+    stepsCounter++;
+    pigPosition++;
+  }
+
+  private void printStepData() {
+    System.out.println(String.format(
+      "-N: %d, position: %d, N: %d",
+      negativeN,
+      pigPosition,
+      positiveN
+    ));
+  }
+
+  private boolean hasPigEscaped() {
+    return pigPosition == positiveN || pigPosition == negativeN;
+  }
+}
---
+++ src/com/tasks/Tasks_from_work/Furios_Pig/Main.java
@@ -0,0 +1,10 @@
+package com.tasks.Tasks_from_work.Furios_Pig;
+
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] args) {
+    FuriosPig pig = new FuriosPig(24);
+    pig.escape();
+  }
+}
