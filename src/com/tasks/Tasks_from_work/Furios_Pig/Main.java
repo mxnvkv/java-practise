03d2a0d0dcc025cfd2af3eb973e18eb624c39f76
Maksym Novikov <maksymn@suntechinnovation.com>
2021-04-03 17:24:03 +0300

    furious-pig: added comment

--- src/com/tasks/Tasks_from_work/Furios_Pig/Main.java
+++ src/com/tasks/Tasks_from_work/Furios_Pig/Main.java
@@ -2,6 +2,7 @@ package com.tasks.Tasks_from_work.Furios_Pig;
 
 public class Main {
   public static void main(String[] args) {
+    // for N = 10, pig will have to make between ~50.5 and ~51.5 steps
     FuriosPig pig = new FuriosPig(10);
     pig.getExpectedSteps();
   }
