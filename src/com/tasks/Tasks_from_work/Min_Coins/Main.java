be850b20da98f69f2240d76eb9017701b832f3d4
Maksym Novikov <maksymn@suntechinnovation.com>
2021-04-06 21:26:22 +0300

    min-coin: calculate and display the counter

--- src/com/tasks/Tasks_from_work/Min_Coins/Main.java
+++ src/com/tasks/Tasks_from_work/Min_Coins/Main.java
@@ -1,29 +1,27 @@
 package com.tasks.Tasks_from_work.Min_Coins;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
   private static final int[] COINS_DENOMINATION = {1, 3, 9, 27, 81};
   private static final int MONEY_COUNT = 100;
 
   public static void main(String[] args) {
-    getMinAmountOfCoins();
+    System.out.println("Minimum amount of coins required: " + getMinAmountOfCoins());
   }
 
-  private static void getMinAmountOfCoins() {
+  private static int getMinAmountOfCoins() {
+    int minAmountCounter = 0;
+
     for (int i = 1; i <= MONEY_COUNT; i++) {
       int amount = i;
-      List<Integer> list = new ArrayList<>();
 
       while (amount > 0) {
-        int maxCoin = getMaxCoinForAmount(amount);
-        list.add(maxCoin);
-        amount -= maxCoin;
-      }
+        amount -= getMaxCoinForAmount(amount);
 
-      System.out.println(i + "$ : " + list);
+        minAmountCounter = minAmountCounter + 1;
+      }
     }
+
+    return minAmountCounter;
   }
 
   private static int getMaxCoinForAmount(int amount) {
