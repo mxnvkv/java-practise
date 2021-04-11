958ba89e1a24dfcf8cbfb0773f5de401bd291de4
Maksym Novikov <maksymn@suntechinnovation.com>
2021-04-11 18:04:40 +0300

    furious-pig: going to end

--- src/com/tasks/Tasks_from_work/Furios_Pig/FuriousPig.java
+++ src/com/tasks/Tasks_from_work/Furios_Pig/FuriousPig.java
@@ -1,6 +1,7 @@
 package com.tasks.Tasks_from_work.Furios_Pig;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class FuriousPig {
@@ -45,6 +46,98 @@ public class FuriousPig {
 
     Double average = list.stream().mapToInt(i -> i).average().orElse(0.0);
     System.out.println("Expected steps: " + average);
+  }
+
+  public void getExpectedStepsV3() {
+    List<List<List<Double>>> list = fillList();
+
+    // s - steps
+    // v - speed
+    // k - position
+
+    for (int s = 0; s < MAX_STEPS; s++) {
+      for (int v = 0; Math.pow(2, v) <= (2 * positiveN); v++) {
+        for (int k = negativeN + 1; k < positiveN; k++) {
+          int index = k + positiveN;
+          double multiplier = 1.0 / 2;
+          double speed = Math.pow(2, v);
+          double nextStepSpeed = k >= 0 ? Math.pow(2, v + 2) : Math.max(speed / 2, 1); // v
+
+          // s + 1 always
+          // v = k >= 0 ? v * 2 : Math.max(v / 2, 1)
+          // k = k + v || k - v
+
+          list.get(s).get(v).remove(index);
+          list.get(s).get(v).add(
+            index,
+            multiplier * list.get(s + 1).get(getSpeedIndex(k, v, list.get(s).get(v))).get(getPositionIndex((int) (index - nextStepSpeed)))
+              + multiplier * list.get(s + 1).get(getSpeedIndex(k, v, list.get(s).get(v))).get(getPositionIndex((int) (index + nextStepSpeed)))
+          );
+          // list.get(s).get(v).add(index, -1.0);
+
+          // list.get(s).get(v).remove(index);
+          // list.get(s).get(v).add(
+          //   index,
+          //   (multiplier * list.get(s + 1).get((int) speed).get((int) (k - speed))
+          //     + multiplier * list.get(s + 1).get((int) speed).get((int) (k + speed)))
+          // );
+        }
+
+        System.out.println(String.format(
+          "Steps: %d, speed: %f, list of positions: %s",
+          s,
+          Math.pow(2, v),
+          list.get(s).get(v)
+        ));
+      }
+    }
+  }
+
+  private List<List<List<Double>>> fillList() {
+    List<List<List<Double>>> list = new ArrayList<>();
+
+    for (int iS = 0; iS <= MAX_STEPS; iS++) {
+      List<List<Double>> listS = new ArrayList<>();
+      list.add(listS);
+      for (int iV = 0; Math.pow(2, iV) <= (2 * positiveN); iV++) {
+        List<Double> listV = new ArrayList<>();
+        list.get(iS).add(listV);
+        for (int iK = negativeN; iK <= positiveN; iK++) {
+          list.get(iS).get(iV).add(0.0);
+        }
+
+        list.get(iS).get(iV).remove(0);
+        list.get(iS).get(iV).add(0, 1.0);
+        list.get(iS).get(iV).remove(list.get(iS).get(iV).size() - 1);
+        list.get(iS).get(iV).add(1.0);
+      }
+    }
+
+    return list;
+  }
+
+  private int getSpeedIndex(int k, int v, List<Double> list) {
+    int index;
+
+    if (k >= 0) {
+      index = Math.min(v + 1, list.size() - 1);
+    } else {
+      index = Math.max(v - 1, 0);
+    }
+
+    return index;
+  }
+
+  private int getPositionIndex(int position) {
+    int indexPosition;
+
+    if (position < 0) {
+      indexPosition = 0;
+    } else {
+      indexPosition = positiveN;
+    }
+
+    return indexPosition;
   }
 
   private void getLeftPath(int pigPosition, int pigSpeed, int stepsCounter) {
--- src/com/tasks/Tasks_from_work/Furios_Pig/Main.java
+++ src/com/tasks/Tasks_from_work/Furios_Pig/Main.java
@@ -2,7 +2,7 @@ package com.tasks.Tasks_from_work.Furios_Pig;
 
 public class Main {
   public static void main(String[] args) {
-    FuriousPig pig = new FuriousPig(2);
-    pig.getExpectedStepsV1();
+    FuriousPig pig = new FuriousPig(1);
+    pig.getExpectedStepsV3();
   }
 }
